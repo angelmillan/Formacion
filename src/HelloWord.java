6b08d573f570feecb9c0ad2e3c1b6b43fe9bc9f1
Ángel Millán Jaén <angelmillan@me.com>
2017-02-09 16:27:17 +0100

    2 commit

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -4,6 +4,7 @@ public class HelloWord {
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
 		System.out.println("Hello Word");
+		System.out.println("tus muertos");
 	}
 
 }
